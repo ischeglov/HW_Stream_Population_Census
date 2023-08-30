c74521eccc6f54aadd7831a25a440631171d75a9
ischeglov <Ivan_mix@mail.ru>
2023-08-30 21:10:09 +0300

    add basic logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long count = persons.stream()
+                .filter(x -> x.getAge() < 18)
+                .count();
+        System.out.println(count);
+
+        List<String> army = persons.stream()
+                .filter(x -> x.getAge() > 18 && x.getAge() < 27 && x.getSex().equals(Sex.MAN))
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(army);
+
+        List<String> workPepole = persons.stream()
+                .filter(x -> (x.getAge() > 18 && x.getAge() < 60 && x.getSex().equals(Sex.WOMAN)
+                    || x.getAge() > 18 && x.getAge() < 65 && x.getSex().equals(Sex.MAN))
+                        && x.getEducation().equals(Education.HIGHER))
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(workPepole);
+    }
 }
--- src/main/java/Sex.java
+++ src/main/java/Sex.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public class Sex {
+public enum Sex {
+    MAN,
+    WOMAN
 }
